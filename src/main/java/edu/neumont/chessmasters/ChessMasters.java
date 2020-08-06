839c902a3805ce4b91bb4e6eb71eec9cf27d50be
DRoberts919 <60489717+DRoberts919@users.noreply.github.com>
2020-08-05 20:47:48 -0600

    User input works
    User input is taken in and validated using regex to make sure it does not go over bounds.

--- src/main/java/edu/neumont/chessmasters/ChessMasters.java
+++ src/main/java/edu/neumont/chessmasters/ChessMasters.java
@@ -1,5 +1,6 @@
 package edu.neumont.chessmasters;
 
+import Controllers.PlayerMove;
 import me.travja.utils.utils.IOUtils;
 
 public class ChessMasters {
@@ -7,9 +8,12 @@ public class ChessMasters {
     public static void main(String[] args) {
 
         //Basic IOUtils use
-        String input = IOUtils.promptForString("Enter a string: ");
-        System.out.println("You input '" + input + "'");
-
+//        String input = IOUtils.promptForString("Enter a string: ");
+//        System.out.println("You input '" + input + "'");
+        PlayerMove.PromptChoice();
     }
+
+
+
 
 }
