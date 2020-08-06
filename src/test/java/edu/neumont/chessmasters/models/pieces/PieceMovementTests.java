23b8361e0a40d50f3f76819e0293cf484e8891c3
Travja <the.only.t.craft@gmail.com>
2020-08-05 20:54:10 -0600

    Finish out all other tests

--- src/test/java/edu/neumont/chessmasters/models/pieces/PieceMovementTests.java
+++ src/test/java/edu/neumont/chessmasters/models/pieces/PieceMovementTests.java
@@ -7,66 +7,58 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class PieceMovementTests {
 
+    private boolean test(Piece piece, String position) {
+        System.out.println("Testing " + piece.getColor().toString() + " " + piece.getClass().getSimpleName() + " moving from " + piece.getLocation().toString() + " to " + position);
+        return piece.move(position);
+    }
+
     @Test
     void testPawnMovement() {
         System.out.println("\nTesting Pawn movement");
         Pawn wpawn = new Pawn(PieceColor.WHITE);
         wpawn.setLocation("b2");
-        System.out.println("Testing white pawn moving from b2 to b4");
-        assert (wpawn.move("b4"));
+        assert (test(wpawn, "b4"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Testing white pawn moving from b4 to b5");
-        assert (wpawn.move("b5"));
+        assert (test(wpawn, "b5"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Testing white pawn moving from b5 to c6");
-        assert (wpawn.move("c6"));
+        assert (test(wpawn, "c6"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Assert we can't move 2 forward now that we've already made a move");
-        assertFalse (wpawn.move("c8"));
+        assertFalse(test(wpawn, "c8"));
         System.out.println("Correct. Invalid move.\n");
 
-        System.out.println("Testing white pawn moving from c6 to d8");
-        assertFalse(wpawn.move("d8"));
+        assertFalse(test(wpawn, "d8"));
         System.out.println("Correct. Invalid move.\n");
 
         wpawn.setLocation("b2");
-        System.out.println("Testing white pawn moving from b2 to b5");
-        assertFalse(wpawn.move("b5"));
+        assertFalse(test(wpawn, "b5"));
         System.out.println("Correct. Invalid move.\n");
 
-        System.out.println("Testing white pawn moving from b2 to b1");
-        assertFalse(wpawn.move("b1"));
+        assertFalse(test(wpawn, "b1"));
         System.out.println("Correct. Invalid move.\n");
 
 
         Pawn bpawn = new Pawn(PieceColor.BLACK);
         bpawn.setLocation("b7");
-        System.out.println("Testing black pawn moving from b7 to b5");
-        assert (bpawn.move("b5"));
+        assert (test(bpawn, "b5"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Testing black pawn moving from b5 to b4");
-        assert (bpawn.move("b4"));
+        assert (test(bpawn, "b4"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Testing black pawn moving from b4 to c3");
-        assert (bpawn.move("c3"));
+        assert (test(bpawn, "c3"));
         System.out.println("Correct. Valid move.\n");
 
-        System.out.println("Testing black pawn moving from c3 to d1");
-        assertFalse(bpawn.move("d1"));
+        assertFalse(test(bpawn, "d1"));
         System.out.println("Correct. Invalid move.\n");
 
         bpawn.setLocation("b7");
-        System.out.println("Testing black pawn moving from b7 to b4");
-        assertFalse(bpawn.move("b4"));
+        assertFalse(test(bpawn, "b4"));
         System.out.println("Correct. Invalid move.\n");
 
-        System.out.println("Testing black pawn moving from b7 to b8");
-        assertFalse(bpawn.move("b8"));
+        assertFalse(test(bpawn, "b8"));
         System.out.println("Correct. Invalid move.\n");
     }
 
@@ -76,30 +68,30 @@ class PieceMovementTests {
         System.out.println("\nTesting that king can move in each of the 8 directions.");
         king.setLocation("d4");
         System.out.println("From d4 to d5");
-        assert (king.move("d5"));
+        assert (test(king, "d5"));
         System.out.println("From d5 to e6");
-        assert (king.move("e6"));
+        assert (test(king, "e6"));
         System.out.println("From e6 to f6");
-        assert (king.move("f6"));
+        assert (test(king, "f6"));
         System.out.println("From f6 to g5");
-        assert (king.move("g5"));
+        assert (test(king, "g5"));
         System.out.println("From g5 to g4");
-        assert (king.move("g4"));
+        assert (test(king, "g4"));
         System.out.println("From g4 to f3");
-        assert (king.move("f3"));
+        assert (test(king, "f3"));
         System.out.println("From f3 to e3");
-        assert (king.move("e3"));
+        assert (test(king, "e3"));
         System.out.println("From e3 to d2");
-        assert (king.move("d2"));
+        assert (test(king, "d2"));
         System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
 
         king.setLocation("d4");
         System.out.println("From d4 to d6");
-        assertFalse(king.move("d6"));
+        assertFalse(test(king, "d6"));
         System.out.println("From d4 to f6");
-        assertFalse(king.move("f6"));
+        assertFalse(test(king, "f6"));
         System.out.println("From d4 to d1");
-        assertFalse(king.move("d1"));
+        assertFalse(test(king, "d1"));
 
         System.out.println("Setting king to g8 and attempting to move off the board.");
         king.setLocation("h8");
@@ -127,160 +119,160 @@ class PieceMovementTests {
         System.out.println("All passed. King moves properly.");
     }
 
-//    @Test
-//    void queenTest() {
-//        Queen queen = new Queen(PieceColor.WHITE);
-//        System.out.println("\nTesting that queen can move in each of the 8 directions.");
-//        queen.setLocation("d4");
-//        System.out.println("From d4 to d5");
-//        assert (queen.move("d5"));
-//        System.out.println("From d4 to e5");
-//        assert (queen.move("e5"));
-//        System.out.println("From d4 to e4");
-//        assert (queen.move("e4"));
-//        System.out.println("From d4 to e3");
-//        assert (queen.move("e3"));
-//        System.out.println("From d4 to d3");
-//        assert (queen.move("d3"));
-//        System.out.println("From d4 to c3");
-//        assert (queen.move("c3"));
-//        System.out.println("From d4 to c4");
-//        assert (queen.move("c4"));
-//        System.out.println("From d4 to c5");
-//        assert (queen.move("c5"));
-//
-//        System.out.println("From d4 to d7");
-//        assert (queen.move("d7"));
-//        System.out.println("From d4 to f6");
-//        assert (queen.move("f6"));
-//        System.out.println("From d4 to f4");
-//        assert (queen.move("f4"));
-//        System.out.println("From d4 to f2");
-//        assert (queen.move("f2"));
-//        System.out.println("From d4 to d2");
-//        assert (queen.move("d2"));
-//        System.out.println("From d4 to b2");
-//        assert (queen.move("b2"));
-//        System.out.println("From d4 to b4");
-//        assert (queen.move("b4"));
-//        System.out.println("From d4 to b6");
-//        assert (queen.move("b6"));
-//        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
-//
-//        System.out.println("From d4 to c6");
-//        assertFalse(queen.move("c6"));
-//        System.out.println("From d4 to e6");
-//        assertFalse(queen.move("e6"));
-//        System.out.println("From d4 to g8");
-//        assertFalse(queen.move("g8"));
-//        System.out.println("All passed. Queen moves properly.");
-//    }
-//
-//    @Test
-//    void bishopTest() {
-//        Bishop bishop = new Bishop(PieceColor.WHITE);
-//        System.out.println("\nTesting that bishop can move in each of the 4 diagonal directions.");
-//        bishop.setLocation("d4");
-//        System.out.println("From d4 to e5");
-//        assert (bishop.move("e5"));
-//        System.out.println("From d4 to e3");
-//        assert (bishop.move("e3"));
-//        System.out.println("From d4 to c3");
-//        assert (bishop.move("c3"));
-//        System.out.println("From d4 to c5");
-//        assert (bishop.move("c5"));
-//
-//        System.out.println("From d4 to f6");
-//        assert (bishop.move("f6"));
-//        System.out.println("From d4 to f2");
-//        assert (bishop.move("f2"));
-//        System.out.println("From d4 to b2");
-//        assert (bishop.move("b2"));
-//        System.out.println("From d4 to b6");
-//        assert (bishop.move("b6"));
-//        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
-//
-//        System.out.println("From d4 to c6");
-//        assertFalse(bishop.move("c6"));
-//        System.out.println("From d4 to e6");
-//        assertFalse(bishop.move("e6"));
-//        System.out.println("From d4 to g8");
-//        assertFalse(bishop.move("g8"));
-//        System.out.println("From d4 to d5");
-//        assertFalse(bishop.move("d5"));
-//        System.out.println("From d4 to f4");
-//        assertFalse(bishop.move("f4"));
-//        System.out.println("All passed. Bishop moves properly.");
-//    }
-//
-//    @Test
-//    void rookTest() {
-//        Rook rook = new Rook(PieceColor.WHITE);
-//        System.out.println("\nTesting that rook can move in each of the 4 cardinal directions.");
-//        rook.setLocation("d4");
-//        System.out.println("From d4 to c4");
-//        assert (rook.move("c4"));
-//        System.out.println("Fromm d4 to e4");
-//        assert (rook.move("e4"));
-//        System.out.println("From d4 to d5");
-//        assert (rook.move("d5"));
-//        System.out.println("From d4 to d3");
-//        assert (rook.move("d3"));
-//        System.out.println("From d4 to d8");
-//        assert (rook.move("d8"));
-//        System.out.println("From d4 to f4");
-//        assert (rook.move("f4"));
-//        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
-//
-//        System.out.println("From d4 to e5");
-//        assertFalse(rook.move("e5"));
-//        System.out.println("From d4 to e3");
-//        assertFalse(rook.move("e3"));
-//        System.out.println("From d4 to c3");
-//        assertFalse(rook.move("c3"));
-//        System.out.println("From d4 to c5");
-//        assertFalse(rook.move("c5"));
-//        System.out.println("All passed. Rook moves properly.");
-//    }
-//
-//    @Test
-//    void knightTest() {
-//        Knight knight = new Knight(PieceColor.WHITE);
-//        System.out.println("\nTesting that knight can move in each of the 8 possible ways.");
-//        knight.setLocation("d4");
-//        System.out.println("From d4 to b5");
-//        assert (knight.move("b5"));
-//        System.out.println("From d4 to c6");
-//        assert (knight.move("c6"));
-//        System.out.println("From d4 to e6");
-//        assert (knight.move("e6"));
-//        System.out.println("From d4 to f5");
-//        assert (knight.move("f5"));
-//        System.out.println("From d4 to f3");
-//        assert (knight.move("f3"));
-//        System.out.println("From d4 to e2");
-//        assert (knight.move("e2"));
-//        System.out.println("From d4 to c2");
-//        assert (knight.move("c2"));
-//        System.out.println("From d4 to b3");
-//        assert (knight.move("b3"));
-//        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
-//
-//        System.out.println("From d4 to e5");
-//        assertFalse(knight.move("e5"));
-//        System.out.println("From d4 to e3");
-//        assertFalse(knight.move("e3"));
-//        System.out.println("From d4 to c3");
-//        assertFalse(knight.move("c3"));
-//        System.out.println("From d4 to c5");
-//        assertFalse(knight.move("c5"));
-//        System.out.println("From d4 to c7");
-//        assertFalse(knight.move("c7"));
-//        System.out.println("From d4 to a8");
-//        assertFalse(knight.move("a8"));
-//        System.out.println("From d4 to a7");
-//        assertFalse(knight.move("a7"));
-//        System.out.println("All passed. Knight moves properly.");
-//    }
+    @Test
+    void queenTest() {
+        Queen queen = new Queen(PieceColor.WHITE);
+        System.out.println("\nTesting that queen can move in each of the 8 directions.");
+        queen.setLocation("d4");
+        System.out.println("From d4 to d5");
+        assert (test(queen, "d5"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to e5");
+        assert (test(queen, "e5"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to e4");
+        assert (test(queen, "e4"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to e3");
+        assert (test(queen, "e3"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to d3");
+        assert (test(queen, "d3"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to c3");
+        assert (test(queen, "c3"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to c4");
+        assert (test(queen, "c4"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to c5");
+        assert (test(queen, "c5"));
+
+        queen.setLocation("d4");
+        System.out.println("From d4 to d7");
+        assert (test(queen, "d7"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to f6");
+        assert (test(queen, "f6"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to f4");
+        assert (test(queen, "f4"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to f2");
+        assert (test(queen, "f2"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to d2");
+        assert (test(queen, "d2"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to b2");
+        assert (test(queen, "b2"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to b4");
+        assert (test(queen, "b4"));
+        queen.setLocation("d4");
+        System.out.println("From d4 to b6");
+        assert (test(queen, "b6"));
+        queen.setLocation("d4");
+        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
+
+        System.out.println("From d4 to c6");
+        assertFalse(test(queen, "c6"));
+        System.out.println("From d4 to e6");
+        assertFalse(test(queen, "e6"));
+        System.out.println("From d4 to g8");
+        assertFalse(test(queen, "g8"));
+        System.out.println("All passed. Queen moves properly.");
+    }
+
+    @Test
+    void bishopTest() {
+        Bishop bishop = new Bishop(PieceColor.WHITE);
+        System.out.println("\nTesting that bishop can move in each of the 4 diagonal directions.");
+        bishop.setLocation("d4");
+        assert (test(bishop, "e5"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "e3"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "c3"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "c5"));
+
+        bishop.setLocation("d4");
+        assert (test(bishop, "f6"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "f2"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "b2"));
+        bishop.setLocation("d4");
+        assert (test(bishop, "b6"));
+        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
+
+        bishop.setLocation("d4");
+        assertFalse(test(bishop, "c6"));
+        assertFalse(test(bishop, "e6"));
+        assertFalse(test(bishop, "g8"));
+        assertFalse(test(bishop, "d5"));
+        assertFalse(test(bishop, "f4"));
+        System.out.println("All passed. Bishop moves properly.");
+    }
+
+    @Test
+    void rookTest() {
+        Rook rook = new Rook(PieceColor.WHITE);
+        System.out.println("\nTesting that rook can move in each of the 4 cardinal directions.");
+        rook.setLocation("d4");
+        assert (test(rook, "c4"));
+        rook.setLocation("d4");
+        assert (test(rook, "e4"));
+        rook.setLocation("d4");
+        assert (test(rook, "d5"));
+        rook.setLocation("d4");
+        assert (test(rook, "d3"));
+        rook.setLocation("d4");
+        assert (test(rook, "d8"));
+        rook.setLocation("d4");
+        assert (test(rook, "f4"));
+        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
+
+        rook.setLocation("d4");
+        assertFalse(test(rook, "e5"));
+        assertFalse(test(rook, "e3"));
+        assertFalse(test(rook, "c3"));
+        assertFalse(test(rook, "c5"));
+        System.out.println("All passed. Rook moves properly.");
+    }
+
+    @Test
+    void knightTest() {
+        Knight knight = new Knight(PieceColor.WHITE);
+        System.out.println("\nTesting that knight can move in each of the 8 possible ways.");
+        knight.setLocation("d4");
+        assert (test(knight, "b5"));
+        knight.setLocation("d4");
+        assert (test(knight, "c6"));
+        knight.setLocation("d4");
+        assert (test(knight, "e6"));
+        knight.setLocation("d4");
+        assert (test(knight, "f5"));
+        knight.setLocation("d4");
+        assert (test(knight, "f3"));
+        knight.setLocation("d4");
+        assert (test(knight, "e2"));
+        knight.setLocation("d4");
+        assert (test(knight, "c2"));
+        knight.setLocation("d4");
+        assert (test(knight, "b3"));
+        System.out.println("All valid movement tests passed. Testing a few invalid moves now.\n");
+
+        knight.setLocation("d4");
+        assertFalse(test(knight, "e5"));
+        assertFalse(test(knight, "e3"));
+        assertFalse(test(knight, "c3"));
+        assertFalse(test(knight, "c5"));
+        assertFalse(test(knight, "c7"));
+        assertFalse(test(knight, "a8"));
+        assertFalse(test(knight, "a7"));
+        System.out.println("All passed. Knight moves properly.");
+    }
 
 }
