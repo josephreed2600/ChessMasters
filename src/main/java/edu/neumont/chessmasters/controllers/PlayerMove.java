35c222967a62ded878f6e9bb8d7917f0acac545c
DRoberts919 <60489717+DRoberts919@users.noreply.github.com>
2020-08-10 20:37:14 -0600

    updated and working
    Fixed the input code so that it takes in lower case and upper case inputs. As well as implemented the forfeit option.

--- src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
+++ src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
@@ -12,23 +12,26 @@ public class PlayerMove {
         System.out.println("Which piece do you want to move based on location {Ex: A1}");
         String positionOne = IOUtils.promptForString("Enter a string: ");
 
+        QuitGame(positionOne);
 
         //System.out.println(positionOne);
 
         System.out.println("Position You want to move piece Ex: A2");
         String positionTwo = IOUtils.promptForString("Enter a string  ");
 
+        QuitGame(positionTwo);
+
         //Checks users input is a valid position.
         CheckMove(positionOne, positionTwo);
     }
 
     public static void CheckMove(String po1, String po2) {
-        boolean checkerOne = Pattern.matches("[A-H][1-8]", po1);
-        boolean checkerTwo = Pattern.matches("[A-H][1-8]", po2);
+        boolean checkerOne = Pattern.matches("[A-Ha-h][1-8]", po1);
+        boolean checkerTwo = Pattern.matches("[A-Ha-h][1-8]", po2);
 
 
         if (checkerOne == true) {
-            System.out.println("You are correct");
+            //System.out.println("You are correct");
 
             if (checkerTwo == true) {
                 System.out.println("both of your inputs are valid");
@@ -39,6 +42,15 @@ public class PlayerMove {
             PromptChoice();
         }
 
+    }
+
+    public static void QuitGame(String s) {
+        String quit = "quit";
+
+        if (s.equals(quit)) {
+            System.out.println("You have given up because you do not want to play");
+            System.exit(0);
+        }
     }
 
 
