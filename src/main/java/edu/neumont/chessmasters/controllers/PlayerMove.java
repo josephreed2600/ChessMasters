4683062d191a4686455f29e3823a82a2b89ddc64
DRoberts919 <60489717+DRoberts919@users.noreply.github.com>
2020-08-13 19:25:27 -0600

    Update PlayerMove.java

--- src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
+++ src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
@@ -11,7 +11,7 @@ public class PlayerMove {
     public static void MenuPrompt() {
 
         StringBuilder sb = new StringBuilder("Welcome to Chess Masters");
-        sb.append("\n\n").append("1)Make move \n").append("2)help").append("3)quit");
+        sb.append("\n\n").append("1)Make move \n").append("2)help").append("\n3)quit");
         System.out.println(sb);
         String input = IOUtils.promptForString("Enter a choice");
 
@@ -36,7 +36,7 @@ public class PlayerMove {
     public static void PromptMove() {
 
         //Prompts user for first location
-        System.out.println("Which piece do you want to move based on location {Ex: A1}");
+        System.out.println("\nWhich piece do you want to move based on location {Ex: A1}");
         String positionOne = IOUtils.promptForString("Enter a string: ");
 
         QuitGame(positionOne);
@@ -57,7 +57,7 @@ public class PlayerMove {
         boolean checkerTwo = Pattern.matches("[A-Ha-h][1-8]", po2);
 
 
-        if (checkerOne  && checkerTwo ) {
+        if (checkerOne && checkerTwo) {
             System.out.println("both of your inputs are valid");
             //Sends both moves to next method to move piece
         } else {
@@ -79,7 +79,7 @@ public class PlayerMove {
     private static void helpMenu() {
         StringBuilder helper = new StringBuilder("Helper commands");
 
-        helper.append("\nWhen inputting a position only give two characters:\n Ex) A2").append("\nTo quit/forfeit the game simply type quit whenever\n");
+        helper.append("\n\nWhen inputting a position only give two characters:\n Ex) A2").append("\nTo quit/forfeit the game simply type quit whenever\n");
         System.out.println(helper);
 
         MenuPrompt();
