ffe3fda0bfce2cf8e77657957d475b66378cc0b9
Joseph Reed <josephreed2600@gmail.com>
2020-08-20 18:16:11 -0600

    Add GameSettings model whoops

---
+++ src/main/java/edu/neumont/chessmasters/models/GameSettings.java
@@ -0,0 +1,19 @@
+package edu.neumont.chessmasters.models;
+
+//import;
+
+public class GameSettings {
+	//public enum ColorSelection { ON, OFF, AUTO }
+	public boolean debug = false;
+	public boolean start = false;
+	public boolean needsWrapping;
+	//public ColorSelection color = ColorSelection.AUTO;
+	public Boolean color = null;
+	public Boolean unicode = null;
+	public String filePath;
+	public String fileContents;
+	public String[] raw;
+	public GameSettings(String[] raw) {
+		this.raw = raw;
+	}
+}
