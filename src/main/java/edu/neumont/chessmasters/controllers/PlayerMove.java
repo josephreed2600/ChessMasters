b78d240308ffd12f9e3a3c60808b16fd5b5adab1
Joseph Reed <josephreed2600@gmail.com>
2020-08-24 06:51:54 -0600

    Rewrite controller

--- src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
+++ src/main/java/edu/neumont/chessmasters/controllers/PlayerMove.java
@@ -1,6 +1,8 @@
 package edu.neumont.chessmasters.controllers;
 
 import edu.neumont.chessmasters.models.Board;
+import edu.neumont.chessmasters.models.Move;
+import edu.neumont.chessmasters.models.GameSettings;
 import edu.neumont.chessmasters.models.pieces.Piece;
 import edu.neumont.chessmasters.models.pieces.PieceColor;
 import me.travja.utils.utils.IOUtils;
@@ -12,49 +14,155 @@ public class PlayerMove {
 
     private        Board      board;
     private static PlayerMove inst;
-    private        int        counter  = 0;
+		// Half-turns, starting at 0. To get turn number, (counter/2) + 1
+    private        int        counter  = -1;
     private        String     status   = null;
     private        boolean    gameOver = false;
     public         String     positionOne;
     public         String     positionTwo;
 
-    public PlayerMove() {
+		public String getColorName() { return getColorName(counter); }
+		public static String getColorName(int halfturn) { return halfturn % 2 == 0 ? "White" : "Black"; }
+
+		public PieceColor getColor() { return getColor(counter); }
+		public static PieceColor getColor(int halfturn) { return halfturn % 2 == 0 ? PieceColor.WHITE : PieceColor.BLACK; }
+
+		public int getTurn() { return getTurn(counter); }
+		public static int getTurn(int halfturn) { return halfturn / 2 + 1; }
+
+		public boolean isWhite() { return isWhite(counter); }
+		public static boolean isWhite(int halfturn) { return halfturn % 2 == 0; }
+
+		public boolean isBlack() { return isBlack(counter); }
+		public static boolean isBlack(int halfturn) { return halfturn % 2 == 1; }
+
+    private PlayerMove() {
         this(new Board());
     }
 
-    public PlayerMove(Board board) {
-        inst = this;
+    private PlayerMove(Board board) {
+        //inst = this;
         this.board = board;
+				this.counter = -1;
+				this.setStatus(null);
+				this.gameOver = false;
     }
 
     public static PlayerMove inst() {
+			if (inst == null) inst = new PlayerMove();
         return inst;
     }
 
-    public Board getBoard() {
-        return board;
-    }
+		public static PlayerMove inst(Board board) {
+			inst = new PlayerMove(board);
+        return inst;
+		}
 
-    public void run() throws EOFException {
+    public Board getBoard() { return board; }
+
+    public void run(GameSettings options) throws EOFException {
         gameOver = false;
         boolean keepPlaying;
         do {
-            keepPlaying = MenuPrompt();
+            keepPlaying = RequestMove();
+						if (this.getStatus() != null) {
+							System.out.println(this.getStatus());
+						}
+						if (this.gameOver) {
+							return;
+						}
         } while (keepPlaying);
     }
 
-    public void setGameOver() {
-        this.gameOver = true;
-    }
+    public void setGameOver() { this.gameOver = true; }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    public void setStatus(String status) { this.status = status; }
 
-    public String getStatus() {
-        return status;
-    }
+    public String getStatus() { return status; }
 
+		/**
+		 * Does not return until a valid move is made or the user forfeits.
+		 * 0. Increment the half-turn counter
+		 * 1. Clear en passant targets for this player
+		 * 2. Print board
+		 * 3. Do-while(not a valid move or a forfeit):
+		 * 3.1 Request user input
+		 * 3.2 Check whether it's a predefined command
+		 * 3.2.1 Execute command
+		 * 3.2.2 Continue if it wasn't a forfeit
+		 * 3.3 Attempt to parse as a move
+		 * 3.3.1 If successful, attempt to apply the resultant move
+		 * 3.3.1.1 If move is successful, return
+		 * 3.3.2 If either of these fails, continue prompting
+		 */
+		public boolean RequestMove() throws EOFException {
+		 // 0. Increment the half-turn counter
+			counter++;
+
+		 // 1. Clear en passant targets for this player
+			board.clearPassant(getColor());
+
+		 // 2. Print board
+			System.out.println("\n\n" + board);
+
+		 // 3. Do-while(not a valid move or a forfeit):
+			do {
+		 // 3.1 Request user input
+				System.out.println(getColorName() + " to move");
+				String input = IOUtils.promptForString(getTurn() + (isWhite() ? ". " : "... "));
+		 // 3.2 Check whether it's a predefined command
+				switch (input) {
+					case "exit":
+		 // 3.2.1 Execute command
+						System.exit(0); // exits application
+					case "forfeit":
+		 // 3.2.1 Execute command
+						System.out.println(getColorName() + " has elected to forfeit. " + (isWhite() ? "0-1" : "1-0"));
+						return false; // exits method, indicating game is over
+					case "help":
+					case "?":
+		 // 3.2.1 Execute command
+						helpMenu();
+						continue; // skips rest of loop
+		 // 3.2.2 Continue if it wasn't a forfeit
+					default: break; // carries on with loop
+				}
+
+				try {
+
+		 // 3.3 Attempt to parse as a move
+					Move move = parseMove(input, board);
+
+		 // 3.3.1 If successful, attempt to apply the resultant move
+					if (AttemptMove(move, board, getColor())) return true; // exits the method, indicating a successful move
+					else {
+							System.err.println("Illegal move");
+		 // 3.3.2 If either of these fails, continue prompting
+							continue;
+					}
+
+				} catch (Exception e) {
+					System.err.println("Invalid move syntax or unrecognized command. Try specifying a source and a destination square, like so: a2 a4");
+		 // 3.3.2 If either of these fails, continue prompting
+					continue;
+				}
+
+			} while (true); // continue looping until something returns true or false
+
+			// we better not get here tho
+			//throw new IllegalStateException("We escaped a do-while(true) in PlayerMove");
+		}
+
+		public Move parseMove(String input) {
+			String[] squares = input.split(" ");
+			Move move = new Move(squares[0], squares[1]);
+			return move;
+		}
+		public Move parseMove(String input, Board board) {
+			return parseMove(input);
+		}
+
+		/*
     //Returns a boolean dependent on if the player intends to quit the game or not.
     public boolean MenuPrompt() throws EOFException {
         board.clearPassant(counter % 2 == 0 ? PieceColor.WHITE : PieceColor.BLACK);
@@ -136,88 +244,83 @@ public class PlayerMove {
         } while (result == MoveResult.FAILED);
         return false;
     }
+*/
 
-    public MoveResult PromptMove() {
-        MoveResult result = MoveResult.FAILED;
+	// true for success, false for failure
+	// 1. Ensure that the source square has a piece
+	// 2. Ensure that the piece to move is of the correct color
+	// 3. Attempt the move
+	public boolean AttemptMove(Move move, Board board, PieceColor color) {
+		Piece p = board.getSquare(move.from);
+	// 1. Ensure that the source square has a piece
+		if (p == null) {
+			System.out.println("The source square (" + move.from.toString() + ") is empty");
+			return false;
+		}
+	// 2. Ensure that the piece to move is of the correct color
+		if (p.getColor() != color) {
+			System.out.println("The requested piece (" + p.getNotation() + move.from.toString() + ") is of the wrong color");
+			return false;
+		}
+	// 3. Attempt the move
+		return board.movePiece(move);
+	}
 
-        //Prompts user for first location
-        boolean checkPiece = true;
-        boolean checkSecond = true;
-        //System.out.println("\nWhich piece do you want to move based on location {Ex: A1}");
-
-
-//        do {
-        //positionOne = IOUtils.promptForString("Enter a string: ");
-        checkPiece = CheckMove(positionOne);
-
-        //checks if position is true (valid location)
-        if (checkPiece) {
-            Piece p = board.getSquare(positionOne);
-
-            if (p == null) {
-                checkPiece = false;
-                setStatus("no piece here");
-                return MoveResult.FAILED;
-            } else {
-                PieceColor colorPiece = p.getColor();
-
-                if (counter % 2 == 0) {
-                    //Checking white piece
-                    if (colorPiece != PieceColor.WHITE) {
-                        checkPiece = false;
-                        setStatus("wrong piece color");
-                        return MoveResult.FAILED;
-                    }
-
-                } else {
-                    //Checking black turn {Piece}
-                    if (colorPiece != PieceColor.BLACK) {
-                        checkPiece = false;
-                        setStatus("wrong piece color");
-                        return MoveResult.FAILED;
-                    }
-
-                }
-            }
-        }
-//        } while (!checkPiece);
-
-
-//        do {
-        //System.out.println("Position You want to move piece Ex: A2");
-        //positionTwo = IOUtils.promptForString("Enter a string  ");
-        checkSecond = CheckMove(positionTwo);
-        if (!checkSecond) {
-            setStatus("You have chosen an incorrect location ");
-            return MoveResult.FAILED;
-        }
-//        } while (!checkSecond);
-
-
-        //System.out.println(positionOne);
-
-
-        if (result != MoveResult.QUIT) {
-            //Checks users input is a valid position.
-            boolean c = board.movePiece(positionOne, positionTwo);
-            result = c ? MoveResult.MOVED : MoveResult.FAILED;
-        }
-
-        return result;
-    }
+// ensure that a piece exists in the source square
+// ensure that piece is of correct color
+// attempt to move the piece
+/*
+	public MoveResult PromptMove() {
+		MoveResult result = MoveResult.FAILED;
+		boolean checkPiece = true;
+		boolean checkSecond = true;
+		checkPiece = CheckMove(positionOne);
+		if (checkPiece) {
+			Piece p = board.getSquare(positionOne);
+			if (p == null) {
+				checkPiece = false;
+				setStatus("no piece here");
+				return MoveResult.FAILED;
+			} else {
+				PieceColor colorPiece = p.getColor();
+				if (counter % 2 == 0) {
+					if (colorPiece != PieceColor.WHITE) {
+						checkPiece = false;
+						setStatus("wrong piece color");
+						return MoveResult.FAILED;
+					}
+				} else {
+					if (colorPiece != PieceColor.BLACK) {
+						checkPiece = false;
+						setStatus("wrong piece color");
+						return MoveResult.FAILED;
+					}
+				}
+			}
+		}
+		checkSecond = CheckMove(positionTwo);
+		if (!checkSecond) {
+			setStatus("You have chosen an incorrect location "); // ????
+			return MoveResult.FAILED;
+		}
+		if (result != MoveResult.QUIT) {
+			boolean c = board.movePiece(positionOne, positionTwo);
+			result = c ? MoveResult.MOVED : MoveResult.FAILED;
+		}
+		return result;
+	}
+	*/
 
     private boolean CheckMove(String po1) {
         boolean checkerOne = Pattern.matches("[A-Ha-h][1-8]", po1);
         return checkerOne;
-
-
     }
 
     private boolean QuitGame(String s) {
         String quit = "quit";
 
         if (s.equals(quit)) {
-            System.out.println("You have given up because you do not want to play");
+            System.out.println("You have chosen to forfeit the game");
             return true;
 //            System.exit(0);
         }
